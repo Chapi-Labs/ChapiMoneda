0bc1d683b574a06c5f0fa57f0be1e44dfc801476
pablo <pablo5_diaz@hotmail.com>
2017-02-03 16:07:04 -0600

    add form, fetch, and production ads

--- src/components/app.js
+++ src/components/app.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import {
-  AppRegistry,
   StyleSheet,
   Image,
   View,
-  Dimensions
+  Dimensions,
+  ToastAndroid,
 } from 'react-native';
-import { Container, Content, Footer, List, Header, Title, ListItem, Button, Icon, Picker, Text, InputGroup, Input } from 'native-base';
+import { Container, Content, Footer, List, 
+    Header, Title, ListItem, Button, Icon, 
+    Picker, Text, InputGroup, Input, Spinner } from 'native-base';
 import theme from '../theme/theme';
 const Item = Picker.Item;
 const { height, width } = Dimensions.get('window');
 const vertical = require('./../../assets/img/background-vertical.png');
 const horizontal = require('./../../assets/img/background-horizontal.png');
 import { AdMobInterstitial, AdMobBanner } from 'react-native-admob';
+import SplashScreen from 'react-native-splash-screen';
 
 export default class App extends Component {
 
@@ -27,14 +30,19 @@ export default class App extends Component {
         margin: height > width ? 120: 80,
         mButton: height > width ? 20: 0,
       },
+      amount: undefined,
+      email: undefined,
       selectedBank: 'bi',
-      selectedType: 'key0',
+      selectedType: 'compraInternet',
+      animating: false,
+      responseMsg: undefined
+
+
     };
     this.setBannerSize = this.setBannerSize.bind(this);
   }
 
   renderDimensions(relayout) {
-    console.log(relayout);
     this.setState({
       layout: {
         height: relayout.height,
@@ -44,25 +52,26 @@ export default class App extends Component {
         mButton: height > width ? 20: 0,
       }
     });
-    console.log(this.state);
+
   }
 
   componentDidMount() {
-    AdMobInterstitial.setTestDeviceID('EMULATOR');
-    AdMobInterstitial.setAdUnitID('ca-app-pub-3940256099942544/1033173712');
+    AdMobInterstitial.setTestDeviceID('ad-unit-newton-labs-chapimoneda');
+    AdMobInterstitial.setAdUnitID('ca-app-pub-5810239506156938/2065751809');
 
     AdMobInterstitial.addEventListener('interstitialDidLoad',
-      () => console.log('interstitialDidLoad event'));
+      () => {});
     AdMobInterstitial.addEventListener('interstitialDidClose',
       this.interstitialDidClose);
     AdMobInterstitial.addEventListener('interstitialDidFailToLoad',
-      () => console.log('interstitialDidFailToLoad event'));
+      () => {});
     AdMobInterstitial.addEventListener('interstitialDidOpen',
-      () => console.log('interstitialDidOpen event'));
+      () => {});
     AdMobInterstitial.addEventListener('interstitialWillLeaveApplication',
-      () => console.log('interstitalWillLeaveApplication event'));
+      () => {});
 
     AdMobInterstitial.requestAd((error) => error && console.log(error));
+    SplashScreen.hide();
   }
 
   componentWillUnmount() {
@@ -70,12 +79,10 @@ export default class App extends Component {
   }
 
   interstitialDidClose() {
-    console.log('interstitialDidClose event');
     AdMobInterstitial.requestAd((error) => error && console.log(error));
   }
 
   showInterstital() {
-    console.log('test');
     AdMobInterstitial.showAd((error) => error && console.log(error));
   }
 
@@ -97,23 +104,61 @@ export default class App extends Component {
             selectedBank: value,
       });
   }
-  onValueChange(value: string) {
+  onAmountChange(value: string) {
     this.setState({
-        selected1: value,
+        amount: value,
     });
+  }
+  onSave() {
+    const valid = this.validateForm();
+    if (valid === true) {
+         this.setState({animating: true});
+   fetch('https://chapimoneda.herokuapp.com/api/notification', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        amount: this.state.amount,
+        email: this.state.email.trim(),
+        bank: this.state.selectedBank,
+        type: this.state.selectedType,
+      })
+    }).then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({animating: false, responseMsg: responseJson});
+        this.showInterstital();
+        if (responseJson === 'updated') {
+            ToastAndroid.show('Se ha actualizado correctamente', ToastAndroid.LONG);
+        }
+        if (responseJson === 'saved') {
+            ToastAndroid.show('Se ha guardado correctamente', ToastAndroid.LONG);
+        }
+      })
+      .catch((error) => {
+        this.setState({animating: false});
+        ToastAndroid.show('Error', ToastAndroid.LONG);
+      });
+    } else {
+        ToastAndroid.show('Falta ingresar información', ToastAndroid.LONG);
+    }
+   
   }   
+
+  validateForm() {
+    return !(this.state.amount === undefined ||
+        this.state.email === undefined);
+  }
 
   render() {
     const { bannerSize } = this.state;
-    console.log(bannerSize);
 
     return (
-            
             <Container
             onLayout={ (event) => { this.renderDimensions(event.nativeEvent.layout); }}
             theme={ theme }
             >
-
               <Image
                 source={this.state.layout.image}
                 style={{
@@ -123,8 +168,8 @@ export default class App extends Component {
                 >   
                  <AdMobBanner
                       bannerSize={this.state.bannerSize}
-                      testDeviceID="EMULATOR"
-                      adUnitID="ca-app-pub-3940256099942544/2934735716"
+                      testDeviceID="ad-newtonlabs"
+                      adUnitID="ca-app-pub-5810239506156938/9941625408"
                     />
                     <View 
                         style={{marginTop: this.state.layout.margin}}
@@ -132,28 +177,43 @@ export default class App extends Component {
                     <ListItem>
                         <InputGroup>
                             <Icon name="money" style={{ color: '#ffffff' }}/>
-                            <Input placeholder="Tipo de cambio" keyboardType="numeric"/>
+                            <Input 
+                                onChangeText= {(amount) => this.setState({amount})}
+                                placeholder="Tipo de cambio" keyboardType="numeric"/>
+                            <Button style={{height: 30,  width: 33, marginRight: 12}} onPress={() => 
+                                alert('Cuando el tipo de cambio ingrese el monto ingresado será notificado')} >
+                                <Icon style={{fontSize: 20}} name="question-circle" />
+                            </Button>
                         </InputGroup>
                     </ListItem>
                     <ListItem>
                         <InputGroup>
                             <Icon name="envelope" style={{ color: '#ffffff' }}/>
-                            <Input placeholder="EMAIL" />
+                            <Input  onChangeText= {(email) => this.setState({email})}
+                                placeholder="EMAIL" />
+                            <Button style={{height: 30, width: 33, marginRight: 12}} onPress={() => 
+                                alert('Correo a enviar notificación')} >
+                                <Icon style={{fontSize: 20}} name="question-circle" />
+                            </Button>
                         </InputGroup>
                     </ListItem>
                     <ListItem>
-                        <Icon name="cogs" style={{ color: '#ffffff' }} />
+                        <Icon name="cogs" style={{ color: '#ffffff' }}/>
                         <Picker
                           style= {{ color: '#ffffff'}}
                           iosHeader="Select one"
                           mode="dropdown"
                           selectedValue={this.state.selectedType}
                           onValueChange={this.onTypeChange.bind(this)} >
-                            <Item label="Compra Internet" value="key0" />
-                            <Item label="Venta Internet" value="key1" />
-                            <Item label="Compra Agencia" value="key2" />
-                            <Item label="Venta Agencia" value="key3" />
+                            <Item label="Compra Internet" value="compraInternet" />
+                            <Item label="Venta Internet" value="ventaInternet" />
+                            <Item label="Compra Agencia" value="compraAgencia" />
+                            <Item label="Venta Agencia" value="ventaAgencia" />
                         </Picker>
+                        <Button style={{height: 30}} onPress={() => 
+                            alert('Tipo de cambio que desea ser notificado')} >
+                            <Icon style={{fontSize: 20}} name="question-circle" />
+                        </Button>
                     </ListItem>
                     <ListItem>
                         <Icon name="university" style={{ color: '#ffffff' }} />
@@ -166,15 +226,18 @@ export default class App extends Component {
                             <Item label="Banco Industrial" value="bi" />
                             <Item label="Banco Promerica" value="promerica" />
                         </Picker>
+                         <Button style={{height: 30}} onPress={() => alert('Banco a revisar')} >
+                            <Icon style={{fontSize: 20}} name="question-circle" />
+                        </Button>
                     </ListItem>
                     </View>
                     <Button block
-                        onPress= { this.showInterstital }
+                        onPress= { this.onSave.bind(this) }
                         style={{ marginLeft: 10, marginRight: 10, marginTop: this.state.layout.mButton }}
                     >
                         Guardar
                     </Button>
-
+                    <Spinner style={{opacity: this.state.animating ? 1.0 : 0.0, alignSelf: 'center'}} animating={true}/>
                     </Image>
             </Container>
     );
